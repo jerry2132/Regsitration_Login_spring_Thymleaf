3b6e6f3b685b32bfde670b0cafdad908892e1336
jerry2132 <sumitsinghrawat34@gmail.com>
2023-11-29 20:37:19 +0530

    commit applied

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -5,57 +5,44 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.*;
 
 import com.example.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
+
 	@Autowired
 	CustomUserDetailsService customUserDetailsServcie;
-	
+
 	@Bean
-	public static PasswordEncoder passwordEncoder()
-	{
+	public static PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	
+
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-	{
-	
-		http.csrf().disable().authorizeHttpRequests().requestMatchers("/registration","/password-request","/reset-password").permitAll()
-		.requestMatchers("home").permitAll().and()
-		.formLogin()
-		.loginPage("/login")
-		.loginProcessingUrl("/login")
-		.defaultSuccessUrl("/home",true).permitAll()
-		.and()
-		.logout()
-		.invalidateHttpSession(true)
-		.clearAuthentication(true)
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-		.logoutSuccessUrl("/login?logout").permitAll();
-		
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.csrf().disable().authorizeHttpRequests()
+				.requestMatchers("/registration", "/password-request", "/reset-password", "/css/**")
+				.permitAll().requestMatchers("home").permitAll().and().formLogin().loginPage("/login")
+				.loginProcessingUrl("/login").defaultSuccessUrl("/home", true).permitAll().and().logout()
+				.invalidateHttpSession(true).clearAuthentication(true)
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
+				.permitAll();
 		return http.build();
 	}
-	
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
-	{
-		
+
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
 		auth.userDetailsService(customUserDetailsServcie).passwordEncoder(passwordEncoder());
-		
+
 	}
-	
-	
-	
-	
 
 }
